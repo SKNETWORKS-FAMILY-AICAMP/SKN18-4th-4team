94769847da01f556ac58f02f4be14280eabc0b82
HJincode <catehj22@gmail.com>
2025-11-05 23:34:38 +0900

    feat: scripts 관련 코멘트 추가

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,2 +1,11 @@
 -- 테이블/인덱스 초기 스키마
--- CREATE EXTENSION vector; 
+
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE DATABASE skn_project;
+\c skn_project;
+
+-- (선택) 기본 계정/권한
+CREATE USER skn WITH PASSWORD 'sknpass';
+GRANT ALL PRIVILEGES ON DATABASE skn_project TO skn;
+

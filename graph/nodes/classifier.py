701450b29cea23a0205f69f63e9ffdfaaf13fb5f
chopin0807@naver.com <junho08075@gmail.com>
2025-11-14 16:30:10 +0900

    add: 허깅페이스 변경 전 가이드 작성

--- graph/nodes/classifier.py
+++ graph/nodes/classifier.py
@@ -1,8 +1,29 @@
 # nodes/classifier.py
 from openai import OpenAI
 from graph.state import SelfRAGState
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+from graph.state import SelfRAGState
 
-client = OpenAI()
+
+class HFModelClient:
+    def __init__(self, model_name: str = "aaditya/Llama3‑OpenBioLLM‑8B"):
+        # 허깅페이스 모델 로딩
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", torch_dtype=torch.bfloat16)
+    
+    def chat(self, prompt: str):
+        # 입력 프롬프트를 토크나이즈
+        inputs = self.tokenizer(prompt, return_tensors="pt").to(self.model.device)
+        
+        # 모델 실행하여 응답 생성
+        outputs = self.model.generate(**inputs, max_new_tokens=200)
+        
+        # 출력 텍스트 디코딩
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+client = OpenAI() # env의 LLM_PROVIDER를 읽어서 판단
+# clinet = HFModelClient() # 허깅페이스
 
 
 def classifier(state: SelfRAGState) -> SelfRAGState:
@@ -35,8 +56,10 @@ def classifier(state: SelfRAGState) -> SelfRAGState:
         model="gpt-5-nano",
         messages=[{"role": "user", "content": prompt}]
     )
+    # res = client.chat(prompt) # 허깅페이스
 
     result = res.choices[0].message.content.strip()
+    # result = res.??? # 메시지 출력 확인
 
     if "의학 무관" in result:
         state["need_quit"] = True

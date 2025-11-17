ceff29c1b52316a1a83cd8ea1e22245764fbcef1
HJincode <catehj22@gmail.com>
2025-11-17 16:20:13 +0900

    feat: BE - 인증관련 백엔드 구현 (urls, views ,forms, service)

--- django_app/accounts/urls.py
+++ django_app/accounts/urls.py
@@ -1,6 +1,40 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
+from .views import AccountsLoginView, AccountsLogoutView, AccountsRegisterView, ProfileImageUploadView
 
 app_name = "accounts"
 urlpatterns = [
-    # core 앱 내의 뷰에 대한 URL 패턴을 여기에 추가하세요.
+    path("login/", AccountsLoginView.as_view(), name="login"),
+    path("logout/", AccountsLogoutView.as_view(), name="logout"),
+    path("register/", AccountsRegisterView.as_view(), name="register"),
+    path("profile-image/", ProfileImageUploadView.as_view(), name="profile-image-upload"),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset_form.html",
+            email_template_name="accounts/password_reset_email.html",
+            subject_template_name="accounts/password_reset_subject.txt",
+            success_url="/accounts/password-reset/done/",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html",
+            success_url="/accounts/password-reset/complete/",
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"),
+        name="password_reset_complete",
+    ),
 ]
